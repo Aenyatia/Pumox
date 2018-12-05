161b5dc102abb538d45ff1c54b86645e213332c6
Aenyatia Nirvalen <aenyatia@gmail.com>
2018-12-05 19:10:37 +0100

    Add company service.

---
+++ Pumox/Services/CompanyService.cs
@@ -0,0 +1,30 @@
+﻿namespace Pumox.Services
+{
+	public class CompanyService
+	{
+		public CompanyService()
+		{
+
+		}
+
+		public void SearchCompany()
+		{
+
+		}
+
+		public void CreateCompany()
+		{
+
+		}
+
+		public void UpdateCompany()
+		{
+
+		}
+
+		public void DeleteCompany()
+		{
+
+		}
+	}
+}
