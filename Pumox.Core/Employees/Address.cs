c748ccfd81f7a0977bfe1bd661183115e6a1dee4
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-01-13 22:11:04 +0100

    Add address to employee.

---
+++ Pumox.Core/Employees/Address.cs
@@ -0,0 +1,28 @@
+﻿using Pumox.Core.Shared;
+using System.Collections.Generic;
+
+namespace Pumox.Core.Employees
+{
+	public class Address : ValueObject
+	{
+		public string Street { get; private set; }
+		public string City { get; private set; }
+
+		private Address(string street, string city)
+		{
+			Street = street;
+			City = city;
+		}
+
+		public static Address Create(string street, string city)
+		{
+			return new Address(street, city);
+		}
+
+		public override IEnumerable<object> GetCompareProperties()
+		{
+			yield return Street;
+			yield return City;
+		}
+	}
+}
